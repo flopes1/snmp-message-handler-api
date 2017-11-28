0f4ef7c406bae8497f4e78d0308ba9053351a565
Fernando Ribeiro <ffr@ecomp.poli.br>
2017-11-27 22:45:04 -0300

    Implementa tratamento de número maior que 127 nas partes do OId.

--- src/main/java/com/poli/snmp/service/SnmpMessageHistoryService.java
+++ src/main/java/com/poli/snmp/service/SnmpMessageHistoryService.java
@@ -41,20 +41,24 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 	public List<SnmpMessageHistory> getAllMessagesHistoryDesc() {
 		return this.snmpMessageDao.getAllMessagesHistoryDesc();
 	}
-	
+
 	@Override
-	public String processSnmpMessage(SnmpMessage snmpMessage) throws NumberFormatException, UnknownHostException, IOException {
+	public String processSnmpMessage(SnmpMessage snmpMessage)
+			throws NumberFormatException, UnknownHostException, IOException {
 
 		String result = "";
 
 		// OID
-		// String oid = "1.3.6.1.2.1.1.1.0";
 		String[] oidParts = snmpMessage.getObjectId().split("\\.");
 		String pubOrPri = snmpMessage.getCommunity(); // "public";
 		String oidValue = Integer.toHexString(0x2b);
 
 		for (int i = 2; i < oidParts.length; i++) {
-			oidValue += StringUtils.leftPad(Integer.toHexString(Integer.decode(oidParts[i])), 2, "0");
+			if (Integer.parseInt(oidParts[i]) > 127) {
+				oidValue += getStringComplexOIDPart(oidParts[i]);
+			} else {
+				oidValue += StringUtils.leftPad(Integer.toHexString(Integer.decode(oidParts[i])), 2, "0");
+			}
 		}
 
 		int oidLength = oidValue.length() / 2;
@@ -64,7 +68,6 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 		String oidTotal = oidType + StringUtils.leftPad(Integer.toHexString(oidLength), 2, "0") + oidValue;
 
 		// VarBind
-
 		String varBindType = Integer.toHexString(0x30);
 
 		String valueField = StringUtils.leftPad(Integer.toHexString(0x05), 2, "0")
@@ -74,7 +77,6 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 				+ oidTotal + valueField;
 
 		// VarBindList
-
 		String varBindListType = Integer.toHexString(0x30);
 
 		String varBindListLength = StringUtils.leftPad(Integer.toHexString(varBindTotal.length() / 2), 2, "0");
@@ -82,51 +84,42 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 		String varBindListTotal = varBindListType + varBindListLength + varBindTotal;
 
 		// Error Index
-
 		String errorIndex = StringUtils.leftPad(Integer.toHexString(0x02), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x01), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x00), 2, "0");
 
 		// Error
-
 		String error = StringUtils.leftPad(Integer.toHexString(0x02), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x01), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x00), 2, "0");
 
 		// Request ID
-
 		String requestID = StringUtils.leftPad(Integer.toHexString(0x02), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x01), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x01), 2, "0");
 
 		// PDU
-
 		String PDU = Integer.toHexString(0xa0)
 				+ StringUtils.leftPad(Integer.toHexString(3 + 3 + 3 + (varBindListTotal.length() / 2)), 2, "0")
 				+ requestID + error + errorIndex + varBindListTotal;
 
 		// Community
-
 		String community = StringUtils.leftPad(Integer.toHexString(0x04), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(toHex(pubOrPri).length() / 2), 2, "0") + toHex(pubOrPri);
 
 		// Version
-
 		String version = StringUtils.leftPad(Integer.toHexString(0x02), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x01), 2, "0")
 				+ StringUtils.leftPad(Integer.toHexString(0x00), 2, "0");
 
 		// Message
-
 		String message = Integer.toHexString(0x30)
 				+ StringUtils.leftPad(Integer.toHexString(3 + (community.length() / 2) + (PDU.length() / 2)), 2, "0")
 				+ version + community + PDU;
 
 		// Primeiro argumento é o nome do host destino
-		// InetAddress addr = InetAddress.getByName("127.0.0.1");
 		InetAddress addr = InetAddress.getByName(snmpMessage.getTargetIp());
 
-		// int port = Integer.parseInt("161");
 		int port = Integer.parseInt(snmpMessage.getTargetPort());
 
 		byte[] msg = hexStringToByteArray(message);
@@ -140,15 +133,6 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 		// Envia a mensagem
 		ds.send(pkg);
 
-		// System.out.println(
-		// "Mensagem enviada para: " + addr.getHostAddress() + "\n" + "Porta: "
-		// + port + "\n" + "Mensagem: ");
-
-		// Cria o DatagramSocket para aguardar mensagens, neste momento o método
-		// fica bloqueando
-		// até o recebimente de uma mensagem
-		// System.out.println("Ouvindo a porta: " + port);
-
 		// Preparando o buffer de recebimento da mensagem
 		byte[] msg2 = new byte[256];
 
@@ -160,28 +144,22 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 
 		String response = new String(pkg2.getData(), pkg.getLength(), pkg2.getLength());
 
-		// System.out.println("Response: " + response);
-
 		result += response;
 
 		// Fecha o DatagramSocket
 		ds.close();
 
+		// Esta Expressão Regular melhora significativamente a resposta, que
+		// para alguns
+		// OIDs vem com caracteres especiais que não são exibidos corretamente.
 		return result.replaceAll("[^\\p{ASCII}]|'|,", "");
 	}
 
 	public static byte[] hexStringToByteArray(String s) {
-
 		int len = s.length();
 		byte[] data = new byte[len / 2];
-		for (int
-
-		i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit
-
-			(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i
-
-					+ 1), 16));
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
 		}
 		return data;
 	}
@@ -190,4 +168,31 @@ public class SnmpMessageHistoryService implements ISnmpMessageHistoryService {
 		return String.format("%x", new BigInteger(1, arg.getBytes(Charset.forName("UTF-8"))));
 	}
 
+	public static String getStringComplexOIDPart(String oidPart) {
+		String result = "";
+
+		int valor = Integer.parseInt(oidPart);
+		int count = 0;
+
+		while (true) {
+			count++;
+			if ((valor / (Math.pow(128, count))) < 1) {
+				break;
+			}
+		}
+
+		int resto = valor;
+		int parteInteira;
+
+		for (int j = count - 1; j > 0; j--) {
+			parteInteira = (int) (resto / Math.pow(128, j));
+			resto -= parteInteira * Math.pow(128, j);
+			int soma = parteInteira + 128;
+			result += StringUtils.leftPad(Integer.toHexString(Integer.decode("" + soma)), 2, "0");
+		}
+
+		result += StringUtils.leftPad(Integer.toHexString(Integer.decode("" + resto)), 2, "0");
+
+		return result;
+	}
 }
